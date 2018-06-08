32b510ba5fd6c60a8fe8e889d70599689c28873d
maplemiao <maplemiao@tencent.com>
2018-06-08 13:04:47 +0800

    feat(index): add vue-router to cache
    in case require('vue-router') afterward error

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let vueServerRender
+let vueRouter
 
 // if global.window exsits, which is a TSW inner variable
 if (typeof global.window === 'object') {
@@ -8,6 +9,8 @@ if (typeof global.window === 'object') {
         window.disable()
     }
 
+    // 在这里加载一下vue-router，防止后面使用的时候require报错
+    vueRouter = require('vue-router')
     vueServerRender = require('vue-server-renderer')
 
     if (typeof windowCopy.enable === 'function') {
